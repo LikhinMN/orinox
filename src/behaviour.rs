9166ea18e7eed3674ef685492f5927706fb41168
Likhin M N <mnlikhin@gmail.com>
2026-04-18 21:21:45 +0530

    feat: add Gossipsub topic subscription to behaviour module

--- src/behaviour.rs
+++ src/behaviour.rs
@@ -3,13 +3,24 @@ use libp2p::identity::Keypair;
 use std::io;
 
 pub type OrinoxBehaviour = gossipsub::Behaviour;
+pub const GOSSIPSUB_TOPIC: &str = "orinox-chat";
 
 pub fn create_behaviour(keypair: &Keypair) -> Result<OrinoxBehaviour, io::Error> {
     let config = gossipsub::ConfigBuilder::default()
         .build()
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
-    gossipsub::Behaviour::new(gossipsub::MessageAuthenticity::Signed(keypair.clone()), config)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))
+    let mut behaviour = gossipsub::Behaviour::new(
+        gossipsub::MessageAuthenticity::Signed(keypair.clone()),
+        config,
+    )
+    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
+
+    let topic = gossipsub::IdentTopic::new(GOSSIPSUB_TOPIC);
+    behaviour
+        .subscribe(&topic)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
+
+    Ok(behaviour)
 }
 
